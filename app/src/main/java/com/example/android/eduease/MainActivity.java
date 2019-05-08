ab9196e2c12b97c6411ae7d691242976ae86f53e
sofia100 <sofiasunam99@gmail.com>
2019-05-08 20:38:21 +0530

    professor activity

--- app/src/main/java/com/example/android/eduease/MainActivity.java
+++ app/src/main/java/com/example/android/eduease/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.android.eduease;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button student,professor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        student=findViewById(R.id.button_student);
+        professor=findViewById(R.id.button_professor);
+
+
+        professor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent= new Intent(getApplicationContext(),PROFESSOR.class);
+                startActivity(intent);
+            }
+        });
     }
 }
