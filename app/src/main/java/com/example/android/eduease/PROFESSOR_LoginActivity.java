03674d4d16a41d0ab0883d8cecf6ba3823db221d
sofia100 <sofiasunam99@gmail.com>
2019-05-09 12:29:46 +0530

    professor login activity (still errors)

---
+++ app/src/main/java/com/example/android/eduease/PROFESSOR_LoginActivity.java
@@ -0,0 +1,149 @@
+package com.example.android.eduease;
+
+import android.content.Intent;
+
+import android.support.annotation.NonNull;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class PROFESSOR_LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+
+    private EditText mEmailView;
+    private EditText mPasswordView;
+    Button mEmailSignInButton;
+    String TAG = "PROFESSOR_LoginActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_professor__login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        // Set up the login form.
+        mEmailView = findViewById(R.id.email);
+        //  populateAutoComplete();
+
+        mPasswordView = findViewById(R.id.password);
+
+       /* mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+*/
+        mEmailSignInButton = findViewById(R.id.email_sign_in_button);
+
+
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+
+    }
+
+   /* @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }*/
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            Intent intent = new Intent(PROFESSOR_LoginActivity.this, PROFESSOR.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(PROFESSOR_LoginActivity.this, "UNABLE TO GO FURTHER", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void attemptLogin() {
+        /*
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);*/
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(PROFESSOR_LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(PROFESSOR_LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
+
+    }
+}
+
