c912d17ff30fcff2c6326c4ec6aa6652c5a79da9
sofia100 <sofiasunam99@gmail.com>
2019-05-15 18:13:35 +0530

    retrieve stp 1.2

--- app/src/main/java/com/example/android/eduease/STUDENT_DASHBOARD.java
+++ app/src/main/java/com/example/android/eduease/STUDENT_DASHBOARD.java
@@ -1,13 +1,10 @@
 package com.example.android.eduease;
 
 import android.content.Intent;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -18,13 +15,14 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class STUDENT_DASHBOARD extends AppCompatActivity {
 String TAG = "STUDENT_DASHBOARD";
     FirebaseDatabase database;
     DatabaseReference myRef;
-
+ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +35,7 @@ String TAG = "STUDENT_DASHBOARD";
         database= FirebaseDatabase.getInstance();
         myRef = database.getReference(subj);//subject
 
+        listView = (ListView) findViewById(R.id.list);
 
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -81,57 +80,21 @@ String TAG = "STUDENT_DASHBOARD";
             timeFrom.add((String) singleUser.get("timeFRom"));
             timeTo.add((String) singleUser.get("timeTo"));
         }
-       /* Toast.makeText(getApplicationContext(),"teacher is "+names.toString(),Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(),"teacher is "+names.toString(),Toast.LENGTH_SHORT).show();
 
-        Toast.makeText(getApplicationContext(),"FROM TIME is "+timeFrom.toString(),Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(),"FROM TIME  is "+timeFrom.toString(),Toast.LENGTH_SHORT).show();
 
         Toast.makeText(getApplicationContext(),"TO TIME is "+timeTo.toString(),Toast.LENGTH_SHORT).show();
-*/
-       // System.out.println(names.toString());
 
-        WordAdapter adapter = new WordAdapter(getActivity(), words, R.color.category_numbers);
+        List<String> combined = new ArrayList<String>();
+        combined.addAll(names);
+        combined.addAll(timeFrom);
+        combined.addAll(timeTo);
 
-        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
-        // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // word_list.xml layout file.
-        ListView listView = (ListView) rootView.findViewById(R.id.list);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.list_item,combined);
 
-        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
-        // {@link ListView} will display list items for each {@link Word} in the list.
         listView.setAdapter(adapter);
 
-        // Set a click listener to play the audio when the list item is clicked on
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                // Release the media player if it currently exists because we are about to
-                // play a different sound file
-                releaseMediaPlayer();
-
-                // Get the {@link Word} object at the given position the user clicked on
-                Word word = words.get(position);
-
-                // Request audio focus so in order to play the audio file. The app needs to play a
-                // short audio file, so we will request audio focus with a short amount of time
-                // with AUDIOFOCUS_GAIN_TRANSIENT.
-                int result = audioManager.requestAudioFocus(audioFocusChangeListener,
-                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-
-                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-                    // We have audio focus now.
-
-                    // Create and setup the {@link MediaPlayer} for the audio resource associated
-                    // with the current word
-                    mediaPlayer = MediaPlayer.create(getActivity(), word.getVid());
-
-                    // Start the audio file
-                    mediaPlayer.start();
-
-                    // Setup a listener on the media player, so that we can stop and release the
-                    // media player once the sound has finished playing.
-                    mediaPlayer.setOnCompletionListener(onCompletionListener);
-                }
-            }
-        });
+        // System.out.println(names.toString());
     }
 }
