ab6fae217fb92630cb1696721bcb507e69611237
Mownika <41233359+Mownika25@users.noreply.github.com>
2019-05-18 11:34:57 +0530

    Add files via upload
    adding student data upload

---
+++ app/src/main/java/com/example/android/eduease/StudentDataUpload.java
@@ -0,0 +1,19 @@
+package com.example.android.eduease;
+
+public class StudentDataUpload {
+    String namestu,stuclg;
+
+
+    StudentDataUpload()
+    {
+        namestu=stuclg=null;
+    }
+    public void setName(String namestu) {
+        this.namestu = namestu;
+    }
+
+    public void setClg(String stuclg) {
+        this.stuclg = stuclg;
+    }
+
+}
